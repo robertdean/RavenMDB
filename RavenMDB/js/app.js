f83b224982987da8d7097bd969051354ec49419c
Robert <rob@inq.me>
2013-05-09 09:22:45 -0400

    Testing youtrack integration #RMDB-2 fixed issue

--- RavenMDB/js/app.js
+++ RavenMDB/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/title/:id', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/', {templateUrl: 'partials/search.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/title/:id', {templateUrl: 'partials/titleview.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
