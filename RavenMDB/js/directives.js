3205269dc7015a622ad4e83810019fc59cdfb6f9
robert dean <robertdean@live.com>
2013-05-23 21:32:46 -0400

    adding pagination

--- RavenMDB/js/directives.js
+++ RavenMDB/js/directives.js
@@ -16,30 +16,69 @@ angular.module('myApp.directives', [])
             templateUrl: 'partials/directives/navBar.html'
         };
     }])
-    .directive('facetControl',['SearchService',function(SearchService){
+    .directive('suggestionControl',['SearchService', function(SearchService) {
         return {
-            scope:{
-                facet: "="                
+            scope: {
+                suggestion: "="
             },
             restrict: "E",
-            templateUrl:"partials/directives/facet.html",
-            link: function(scope,element, attributes){
-                scope.setFacet = function(facet,facetValue){
-                    SearchService.setFacet(facet,facetValue);
-                };                
+            templateUrl: "partials/directives/suggestion.html",
+            link: function(scope, element, attributes) {
+                    scope.setSuggestion = function(suggestion) {
+                        SearchService.searchTerms = suggestion;
+                        SearchService.search(suggestion);
+                    };
             }
-        }
+        };
+    }])
+    .directive('facetControl',['SearchService',function(SearchService) {
+        return {
+            scope: {
+                facet: "="
+            },
+            restrict: "E",
+            templateUrl: "partials/directives/facet.html",
+            link: function(scope, element, attributes) {
+                scope.setFacet = function(facet, facetValue) {
+                    SearchService.setFacet(facet, facetValue);
+                };
+            }
+        };
+    }])
+    .directive('pagination', ['SearchService', function (SearchService) {
+        return {
+            scope: {
+                totalPages: "=",
+                currentPage: "="
+            },
+            restrict: "E",
+            link: function (scope, element, attributes) {
+                scope.pages = function () {                    
+                    return SearchService.getPages();
+                };
+                scope.currentPage = function() {
+                    return SearchService.getCurrenPage();
+                };
+                
+                scope.totalRecords = function () {
+                    return SearchService.totalRecords();
+                };
+                
+                
+            },
+            templateUrl: 'partials/directives/pagination.html'
+        };
     }])
     .directive('searchBox', ['SearchService', function (SearchService) {        
         return {
             restrict: "E",
             link: function (scope, element, attributes) {
-                scope.processing  = function(){
+                scope.searchTerms = SearchService.searchTerms;
+                scope.processing = function () {
                       return SearchService.processing();
                 };
                 scope.submit = function () {
                     SearchService.search(scope.searchTerms);
-                    
                 };
             },
             templateUrl: 'partials/directives/searchBox.html'
